c557daf1ac64c836d427b513d4b3216ee77b42b8
keefertaylor <keefer.t@gmail.com>
2019-02-18 22:27:18 +0000

    Force unwrap an optional that will always succeed in order to create a clean API

--- Base58Swift/Base58Check.swift
+++ Base58Swift/Base58Check.swift
@@ -1,7 +1,7 @@
 // Copyright Keefer Taylor, 2019
 
-import Foundation
 import CommonCrypto
+import Foundation
 
 /**
  * A static utility class which provides Base58Check utility functions.
@@ -12,37 +12,29 @@ public class Base58Check {
 
   /** Append a checksum to the given input string. */
   public static func appendChecksum(_ input: String) -> String? {
-    guard let outputBytes = appendChecksum(Array(input.utf8)) else {
-      return nil
-    }
+    let outputBytes = appendChecksum(Array(input.utf8))
     return String(bytes: outputBytes, encoding: .utf8)
   }
 
   /** Append a checksum to the given input byte array. */
-  public static func appendChecksum(_ input: [UInt8]) -> [UInt8]? {
-    guard let checksum = calculateChecksum(input) else {
-      return nil
-    }
+  public static func appendChecksum(_ input: [UInt8]) -> [UInt8] {
+    let checksum = calculateChecksum(input)
     return input + checksum
   }
 
   /**
    * Calculate a checksum for a given input by hashing twice and then taking the first four bytes.
    */
-  private static func calculateChecksum(_ input: [UInt8]) -> [UInt8]? {
-    guard let hashedData = sha256(Data(input)),
-      let doubleHashedData = sha256(hashedData) else {
-        return nil
-    }
+  private static func calculateChecksum(_ input: [UInt8]) -> [UInt8] {
+    let hashedData = sha256(Data(input))
+    let doubleHashedData = sha256(hashedData)
     let doubleHashedArray = Array(doubleHashedData)
     return Array(doubleHashedArray.prefix(checksumLength))
   }
 
   /** Create a sha256 hash of the given data. */
-  private static func sha256(_ data: Data) -> Data? {
-    guard let res = NSMutableData(length: Int(CC_SHA256_DIGEST_LENGTH)) else {
-      return nil
-    }
+  private static func sha256(_ data: Data) -> Data {
+    let res = NSMutableData(length: Int(CC_SHA256_DIGEST_LENGTH))!
     CC_SHA256(
       (data as NSData).bytes,
       CC_LONG(data.count),
