870a68209106b3ce2994a767951401e20718c8d9
keefertaylor <keefer.t@gmail.com>
2019-10-27 10:15:34 -0600

    add package manager support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "Base58Swift",
+    products: [
+        .library(
+            name: "Base58Swift",
+            targets: ["Base58Swift"]),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/attaswift/BigInt.git", from: "3.1.0")
+    ],
+    targets: [
+        .target(
+            name: "Base58Swift",
+            dependencies: ["BigInt"],
+            path: "Base58Swift"),
+        .testTarget(
+            name: "base58swiftTests",
+            dependencies: ["Base58Swift"],
+            path: "Base58SwiftTests"),
+    ]
+)
