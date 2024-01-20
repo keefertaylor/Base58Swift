b6b8b5bce1435be6c1fada2d4083c7a0ecb39a94
Alim Yuzbashev <45521753+alimyuz@users.noreply.github.com>
2024-01-20 17:18:22 +0100

    add support for Linux

--- Package.swift
+++ Package.swift
@@ -1,20 +1,27 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "Base58Swift",
+    platforms: [
+        .macOS(.v10_15),
+    ],
     products: [
         .library(
             name: "Base58Swift",
             targets: ["Base58Swift"])
     ],
     dependencies: [
-      .package(url: "https://github.com/attaswift/BigInt.git", from: "5.0.0")
+      .package(url: "https://github.com/attaswift/BigInt.git", from: "5.0.0"),
+      .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
     ],
     targets: [
         .target(
             name: "Base58Swift",
-            dependencies: ["BigInt"],
+            dependencies: [
+                "BigInt",
+                .product(name: "Crypto", package: "swift-crypto")
+            ],
             path: "Base58Swift"),
         .testTarget(
             name: "base58swiftTests",
